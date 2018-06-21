d5b38440abdd4834f2fc6a82f001b73a59778221
Areeta Wong <areetaw@gmail.com>
2018-06-21 23:24:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-//Code your solution here
+// Code your solution here
+
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('div.target');
+}
+
+function increaseRankBy(n) {
+  const ranked = document.querySelectorAll('.ranked-list');
+  for(let i=0; i < ranked.length; i++) {
+    ranked[i].innerHTML = parseInt(ranked[i]) + n;
+  }
+}
+
+function deepestChild(){
+  const ans = document.getElementById("grand-node").querySelectorAll("div")
+  return ans[3]
+}
