714af52b0b99c5a4218cc728d9a7d382e02dda5b
axaud <aarmientaa@gmail.com>
2018-06-21 23:30:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 //Code your solution here
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector("#nested .target");
+}
+
+function increaseRankBy(n) {
+  var ranks = document.querySelectorAll('.ranked-list');
+  for (var i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  const a = document.getElementById("grand-node").querySelectorAll("div")
+  return a[3]
+}
