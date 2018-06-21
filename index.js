40b4e9636c8759c10d989c36777a4b8e690eadb8
Cassie Areff <cassieareff@gmail.com>
2018-06-21 23:21:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 //Code your solution here
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('div.target');
+}
+function increaseRankBy(n){
+  const rankedList = document.querySelectorAll('.ranked-list')
+  for(let i = 0; i < rankedList.length; i++){
+    rankedList[i].innerHTML = parseInt(rankedList[i]) + n;
+  }
+}
+function deepestChild(){
+  const a = document.getElementById("grand-node").querySelectorAll("div")
+  return a[3]
+}
