b4d999f56f11989d3fdd5def14b145e60c100324
Vivian Phung <vivianphung@outlook.com>
2018-07-13 21:35:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 //Code your solution here
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div')
+}
+
+function increaseRankBy(n) {
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list')
+   
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (i * n).toString()
+  }
+}
